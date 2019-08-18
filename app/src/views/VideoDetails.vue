b9174b658d82d8367f1e9a8d5a10ea39c39d5968
boi123212321 <46821810+boi123212321@users.noreply.github.com>
2019-08-18 23:58:53 +0100

    animation fix

--- app/src/views/VideoDetails.vue
+++ app/src/views/VideoDetails.vue
@@ -322,6 +322,14 @@ export default class VideoDetails extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.15s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
 .topbar {
   position: absolute;
   width: 100%;
