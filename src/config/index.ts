2844fe51762d30c2172ba940d760a03739090f80
boio <xorofficial@gmail.com>
2019-10-02 21:07:30 +0200

    read/create config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,16 @@
-import { readFileSync } from "fs";
+import { readFileSync, existsSync } from "fs";
 
-export default JSON.parse(readFileSync("config.json", "utf-8"))
+let config: any;
+
+if (existsSync("config.json")) {
+  config = JSON.parse(readFileSync("config.json", "utf-8"))
+}
+else {
+  config = {
+    "FFMPEG_PATH": "__SET__HERE__",
+    "FFPROBE_PATH": "__SET__HERE__",
+    "THUMBNAIL_INTERVAL": 60
+  };
+}
+
+export default config;
