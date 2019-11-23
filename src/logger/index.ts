e46475a10540fcc8695b589995c75feabf445228
boio <xorofficial@gmail.com>
2019-11-23 21:09:33 +0100

    Disable folder scan on startup + config option

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -3,11 +3,13 @@ import debug from "debug";
 if (process.env.NODE_ENV == "development") {
   debug.enable("porn:*");
 } else {
-  debug.enable("porn:warn,porn:error");
+  debug.enable("porn:warn,porn:error,porn:message");
 }
 
 export const log = debug("porn:log");
 export const success = debug("porn:success");
-export const warn = debug("porn:warn");
 export const http = debug("porn:http");
+
+export const warn = debug("porn:warn");
 export const error = debug("porn:error");
+export const message = debug("porn:message");
