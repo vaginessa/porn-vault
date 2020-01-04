0a81a157d1915de6617c1d9497e83a1d564b9a4f
boio <xorofficial@gmail.com>
2020-01-04 23:00:45 +0100

    studio page scene query fix

--- app/src/views/StudioDetails.vue
+++ app/src/views/StudioDetails.vue
@@ -312,8 +312,8 @@ export default class StudioDetails extends Vue {
 
       const result = await ApolloClient.query({
         query: gql`
-          query($query: String, $auto: Boolean) {
-            getScenes(query: $query, auto: $auto) {
+          query($query: String) {
+            getScenes(query: $query) {
               ...SceneFragment
               actors {
                 ...ActorFragment
@@ -328,8 +328,7 @@ export default class StudioDetails extends Vue {
           ${actorFragment}
         `,
         variables: {
-          query,
-          auto: true
+          query
         }
       });
 
