06885d483dbaac01a78100156f7f38d97725a86d
boio <xorofficial@gmail.com>
2020-01-10 19:02:57 +0100

    Fixed image swipe direction

--- app/src/components/Lightbox.vue
+++ app/src/components/Lightbox.vue
@@ -8,8 +8,8 @@
     <div style="position: relative; width: 100%; height: 100%;">
       <v-img
         v-touch="{
-          left: decrementIndex,
-          right: incrementIndex,
+          left: incrementIndex,
+          right: decrementIndex,
         }"
         contain
         class="image"
