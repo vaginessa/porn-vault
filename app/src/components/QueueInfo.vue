ffb558636ff6fedd7b39684b7a6c2de54695db61
boio <xorofficial@gmail.com>
2020-01-11 13:06:38 +0100

    Moved processing queue & changed 'About' layout

--- app/src/components/QueueInfo.vue
+++ app/src/components/QueueInfo.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="mt-4 text-center" v-if="info">
-    <div class="title">Processing video queue</div>
-    <div v-if="info.length > 0">
-      <v-progress-circular indeterminate></v-progress-circular>
-    </div>
-    <div>Queue length: {{ info.length }}</div>
-  </div>
+  <v-card v-if="info" class="mb-3" style="border-radius: 10px">
+    <v-card-title>
+      <v-icon medium class="mr-2">mdi-progress-wrench</v-icon>Processing video queue
+    </v-card-title>
+
+    <v-card-text>
+      <div class="my-4">
+        <span class="mr-2 d-inline-block display-1">{{ info.length }}</span>
+        <span class="subtitle-1">videos</span>
+        <span v-if="info.length > 0" class="ml-3">
+          <v-progress-circular size="20" width="2" indeterminate></v-progress-circular>
+        </span>
+      </div>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script lang="ts">
--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -1,23 +1,6 @@
 <template>
   <v-container fluid>
-    <div class="text-center">
-      <div class="pa-3 d-inline-block" style="border: 1px solid #ddd">
-        <div class="title">porn-manager (name TBD)</div>
-
-        <div class="med--text">by boi123212321</div>
-
-        <v-btn
-          depressed
-          href="https://github.com/boi123212321/porn-manager"
-          target="_blank"
-          color="accent mt-3"
-          :class="`text-none ${$vuetify.theme.dark ? 'black--text' : 'white--text'}`"
-        >
-          <v-icon left>mdi-github-circle</v-icon>GitHub
-        </v-btn>
-      </div>
-    </div>
-
+    <div class="text-center headline mt-4">Settings</div>
     <v-row>
       <v-col :cols="12" :sm="6">
         <div>
@@ -55,22 +38,37 @@
     </v-row>
 
     <div>
+      <div class="text-center headline mb-4">Custom data fields</div>
       <CustomFieldCreator />
     </div>
 
-    <QueueInfo />
+    <div class="text-center">
+      <div class="mt-3 pa-3 d-inline-block" style="border: 1px solid #ddd; border-radius: 8px">
+        <div class="title">porn-manager (name TBD)</div>
+
+        <div class="med--text">by boi123212321</div>
+
+        <v-btn
+          depressed
+          href="https://github.com/boi123212321/porn-manager"
+          target="_blank"
+          color="accent mt-3"
+          :class="`text-none ${$vuetify.theme.dark ? 'black--text' : 'white--text'}`"
+        >
+          <v-icon left>mdi-github-circle</v-icon>GitHub
+        </v-btn>
+      </div>
+    </div>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import QueueInfo from "../components/QueueInfo.vue";
 import CustomFieldCreator from "../components/CustomFieldCreator.vue";
 import { contextModule } from "../store/context";
 
 @Component({
   components: {
-    QueueInfo,
     CustomFieldCreator
   }
 })
