05103871c835d6d9df98373fe7e05450d6f93d63
tltgt <tltgt@example.com>
2020-01-18 01:58:40 +1000

    Fix auto resume

--- app/src/views/SceneDetails.vue
+++ app/src/views/SceneDetails.vue
@@ -1011,7 +1011,7 @@ export default class SceneDetails extends Vue {
       "visibilitychange",
       () => {
         if (this.dp && contextModule.scenePauseOnUnfocus) {
-          if (document.hidden) {
+          if (document.hidden && !this.dp.video.paused) {
             this.dp.pause();
             this.autoPaused = true;
             this.dp.notice("Auto pause", 4000, 0.8);
