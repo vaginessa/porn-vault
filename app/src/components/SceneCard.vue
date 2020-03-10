28823af7e0137602aa534d71b63f536cb4e68b82
boio <xorofficial@gmail.com>
2020-03-10 17:34:34 +0100

    #388 explicitly clear video src on mouseleave

--- app/src/components/SceneCard.vue
+++ app/src/components/SceneCard.vue
@@ -154,6 +154,8 @@ export default class SceneCard extends Mixins(SceneMixin) {
   }
 
   mouseleave() {
+    // @ts-ignore
+    this.$refs.video.setAttribute("src", "");
     if (this.playInterval) {
       // console.log("stopping video");
       clearInterval(this.playInterval);
@@ -194,6 +196,7 @@ export default class SceneCard extends Mixins(SceneMixin) {
 .video-insert {
   position: absolute;
   background-size: cover;
+  width: 100%;
   height: 100%;
   overflow: hidden;
   object-fit: cover;
