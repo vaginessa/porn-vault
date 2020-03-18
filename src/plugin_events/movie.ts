a51299eccb38dc6a228562523cf214f3c9da3f39
boio <xorofficial@gmail.com>
2020-03-18 01:05:12 +0100

    add description field to movie plugin result

--- src/plugin_events/movie.ts
+++ src/plugin_events/movie.ts
@@ -62,13 +62,16 @@ export async function onMovieCreate(movie: Movie, event = "movieCreated") {
   )
     movie.backCover = pluginResult.backCover;
 
-    if (
-      typeof pluginResult.spineCover == "string" &&
-      pluginResult.spineCover.startsWith("im_")
-    )
-      movie.spineCover = pluginResult.spineCover;
+  if (
+    typeof pluginResult.spineCover == "string" &&
+    pluginResult.spineCover.startsWith("im_")
+  )
+    movie.spineCover = pluginResult.spineCover;
 
   if (typeof pluginResult.name === "string") movie.name = pluginResult.name;
+
+  if (typeof pluginResult.description === "string")
+    movie.description = pluginResult.description;
 
   if (typeof pluginResult.releaseDate === "number")
     movie.releaseDate = new Date(pluginResult.releaseDate).valueOf();
