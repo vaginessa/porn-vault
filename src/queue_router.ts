b0859ea70a183ad2081b5c4aba86883636c55d45
boio <xorofficial@gmail.com>
2020-04-11 20:18:32 +0200

    adjust queue DB calls

--- src/queue_router.ts
+++ src/queue_router.ts
@@ -15,41 +15,39 @@ router.delete("/:id", async (req, res) => {
 
 router.post("/:id", async (req, res) => {
   await removeSceneFromQueue(req.params.id);
-  if (req.body.scene) {
-    const scene = await Scene.getById(req.params.id);
-    if (scene) {
+  const scene = await Scene.getById(req.params.id);
+
+  if (scene) {
+    if (req.body.scene) {
       Object.assign(scene, req.body.scene);
       logger.log("Merging scene data:", req.body.scene);
       await sceneCollection.upsert(req.params.id, scene);
     }
-  }
-  if (req.body.images) {
-    for (const image of req.body.images) {
-      logger.log("New image!", image);
-      await imageCollection.upsert(image._id, image);
-      await indexImages([image]);
-      const scene = await Scene.getById(image.scene);
-      if (scene) {
+    if (req.body.images) {
+      for (const image of req.body.images) {
+        logger.log("New image!", image);
+        await imageCollection.upsert(image._id, image);
+        await indexImages([image]);
         const actors = await Scene.getActors(scene);
         const labels = await Scene.getLabels(scene);
         await Image.setActors(
           image,
-          actors.map(a => a._id)
+          actors.map((a) => a._id)
         );
         await Image.setLabels(
           image,
-          labels.map(a => a._id)
+          labels.map((a) => a._id)
         );
       }
     }
-  }
-  if (req.body.thumbs) {
-    for (const thumb of req.body.thumbs) {
-      logger.log("New thumbnail!", thumb);
-      // await database.insert(database.store.images, thumb);
-      await imageCollection.upsert(thumb._id, thumb);
+    if (req.body.thumbs) {
+      for (const thumb of req.body.thumbs) {
+        logger.log("New thumbnail!", thumb);
+        await imageCollection.upsert(thumb._id, thumb);
+      }
     }
   }
+
   res.json(null);
 });
 
