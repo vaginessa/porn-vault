955bc5488e064f1a7f18d460667dab127fcf4620
Christopher Caroni <christophercaroni97@gmail.com>
2020-05-30 18:31:32 +0200

    chore: add eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "standard",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 11,
+    project: ["./tsconfig.json"],
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "simple-import-sort"],
+  root: true,
+  rules: {
+    // Turn off default import rules
+    "sort-imports": "off",
+    "import/order": "off",
+    // Use simple-import-sort instead
+    "simple-import-sort/sort": "error",
+  },
+};
