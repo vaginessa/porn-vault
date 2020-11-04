ce2350dac70fb96628f9265a266b833dc8144644
boi123212321 <46821810+boi123212321@users.noreply.github.com>
2020-11-04 15:58:44 +0100

    Update src/routers/static.ts
    Co-authored-by: Christopher Caroni <christophercaroni97@gmail.com>

--- src/routers/static.ts
+++ src/routers/static.ts
@@ -8,7 +8,7 @@ export function applyFlagRoute(app: Application): void {
   });
 }
 
-export function applyStaticRoutes(app: Application) {
+export function applyStaticRoutes(app: Application): void {
   app.use("/js", express.static("./app/dist/js"));
   app.use("/css", express.static("./app/dist/css"));
   app.use("/fonts", express.static("./app/dist/fonts"));
