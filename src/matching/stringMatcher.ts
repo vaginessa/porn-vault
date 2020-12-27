285b7d6ee8b9dbb05705a463e260bcfcd86ee60a
boi123212321 <46821810+boi123212321@users.noreply.github.com>
2020-12-27 22:32:45 +0100

    string matcher logging

--- src/matching/stringMatcher.ts
+++ src/matching/stringMatcher.ts
@@ -1,5 +1,5 @@
 import { StringMatcherOptions } from "../config/schema";
-import * as logger from "../utils/logger";
+import { formatMessage, logger } from "../utils/logger";
 import { ignoreSingleNames, isRegex, Matcher, MatchSource, REGEX_PREFIX } from "./matcher";
 
 export function stripStr(str: string): string {
@@ -19,27 +19,28 @@ export class StringMatcher implements Matcher {
     getInputs: (matchSource: T) => string[],
     sortByLongestMatch?: boolean
   ): T[] {
+    logger.verbose(`(String matcher) Filtering ${itemsToMatch.length} items using term "${str}"`);
     const cleanStr = stripStr(str);
-    // logger.log(`Checking if ${item.name} matches ${str}`);
 
     const matches = itemsToMatch.filter((source) => {
       const inputs = getInputs(source);
       const filteredInputs = this.options.ignoreSingleNames ? ignoreSingleNames(inputs) : inputs;
 
       for (const input of filteredInputs) {
+        logger.silly(`(String matcher) Checking if "${input}" matches "${cleanStr}"`);
         if (isRegex(input)) {
-          logger.log(`Regex: "${input}"`);
-          const isMatch = new RegExp(input.replace(REGEX_PREFIX, ""), "i").test(cleanStr);
+          const regex = input.replace(REGEX_PREFIX, "");
+          const isMatch = new RegExp(regex, "i").test(cleanStr);
+          logger.silly(`(String matcher) Regex: "${regex}", executing regex`);
           if (isMatch) {
             return true;
           }
         } else {
+          logger.silly(`(String matcher) Not a regex, doing substring search`);
           const cleanInput = stripStr(input);
           const matchIndex = cleanStr.indexOf(cleanInput);
-
-          if (matchIndex !== -1) {
-            return true;
-          }
+          logger.silly(`(String matcher) Substring index: ${matchIndex}`);
+          return matchIndex !== -1;
         }
       }
 
@@ -47,9 +48,11 @@ export class StringMatcher implements Matcher {
     });
 
     if (sortByLongestMatch) {
+      logger.debug(`(String matcher) Sorting results by longest match`);
       matches.sort((a, b) => b.name.length - a.name.length);
     }
 
+    logger.verbose(`(String matcher) Matched ${matches.length} items`);
     return matches;
   }
 
@@ -58,6 +61,7 @@ export class StringMatcher implements Matcher {
     str: string,
     getInputs: (matchSource: T) => string[]
   ): boolean {
+    logger.silly(`(String matcher) Checking if ${str} matches item ${formatMessage(item)}`);
     const res = this.filterMatchingItems([item], str, getInputs);
     return !!res.length;
   }
