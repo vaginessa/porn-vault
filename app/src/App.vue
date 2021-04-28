f4bd840240722d6ea03b8577b48bde462eda75bc
boi123212321 <46821810+boi123212321@users.noreply.github.com>
2021-04-28 21:23:25 +0200

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -229,7 +229,7 @@ export default class App extends Vue {
       // @ts-ignore
       this.$vuetify.theme.dark = darkModeLocalStorage == "true";
     } else {
-      this.$vuetify.theme.dark = window.matchMedia?.("(prefers-color-scheme: dark)").matches;
+      this.$vuetify.theme.dark = !!window.matchMedia?.("(prefers-color-scheme: dark)").matches;
     }
 
     const fillActorCardsLocalStorage = localStorage.getItem("pm_fillActorCards");
