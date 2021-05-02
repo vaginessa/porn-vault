0758db4b7a86ade715ee078ac8dec9b60775f25f
boi12321 <46821810+boi123212321@users.noreply.github.com>
2021-05-02 21:30:19 +0200

    add studio extract test case

---
+++ test/matching/studio.spec.ts
@@ -0,0 +1,55 @@
+import { expect } from "chai";
+
+import { studioCollection } from "../../src/database";
+import { extractStudios } from "../../src/extractor";
+import Studio from "../../src/types/studio";
+import { startTestServer, stopTestServer } from "../testServer";
+
+describe("matcher", () => {
+  describe("studio", () => {
+    after(() => {
+      stopTestServer();
+    });
+
+    it("Should correctly match studios", async function () {
+      await startTestServer.call(this);
+
+      const vixen = new Studio("VIXEN");
+
+      const blacked = new Studio("BLACKED");
+
+      await studioCollection.upsert(vixen._id, vixen);
+      await studioCollection.upsert(blacked._id, blacked);
+
+      expect(await studioCollection.count()).to.equal(2);
+
+      {
+        const matches = await extractStudios(
+          "/videos/Networks/VIXEN Media Group/BLACKED/BLACKED - S2018E0520 - Alina Lopez - Side Chick Games 2.mp4"
+        );
+        expect(matches[0]).to.equal(blacked._id);
+      }
+
+      // Add network
+
+      // TODO: add this test
+
+      /*   const vixenNetwork = new Studio("VIXEN Media Group");
+      vixen.parent = vixenNetwork._id;
+      blacked.parent = vixenNetwork._id;
+      await studioCollection.upsert(vixenNetwork._id, vixenNetwork);
+      await studioCollection.upsert(vixen._id, vixen);
+      await studioCollection.upsert(blacked._id, blacked);
+
+      expect(await studioCollection.count()).to.equal(3);
+      expect((await Studio.getSubStudios(vixenNetwork._id)).length).to.equal(2);
+
+      {
+        const matches = await extractStudios(
+          "/videos/Networks/VIXEN Media Group/BLACKED/BLACKED - S2018E0520 - Alina Lopez - Side Chick Games 2.mp4"
+        );
+        expect(matches[0]).to.equal(blacked._id);
+      } */
+    });
+  });
+});
